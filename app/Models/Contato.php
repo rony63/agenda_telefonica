49a3fb97a6d22524c0f8718031e567e57ce0bcd6
rony63 <ronymcall@gmail.com>
2024-05-15 14:28:51 -0300

    fazendo as setter

--- app/Models/Contato.php
+++ app/Models/Contato.php
@@ -16,19 +16,69 @@ class Contato extends Model
 
     ];
 
+    public function getEnderecoAttribute()
+    {
+        return $this->enderecoRelationship;
+    }
+
+    public function getTelefoneAttribute()
+    {
+        return $this->telefoneRelationship;
+    }
+
+    public function getCategoriaAttribute()
+    {
+        return $this->categoriaRelationship;
+    }
+
+    public function setCategoriaAttribute($value)
+    {
+        $this->categoriaRelationship()->sync($value);
+    }
+
+    public function setEnderecoAttribute($value)
+    {
+        if(isset($value))
+        {
+            $this->atributes['endereco_id'] = Endereco::where('id', $value)->first()->id;
+        }
+    }
+
+    public function setTelefoneAttrubute($value)
+    {
+        if(isset($value))
+        {
+            $this->atributes['contato_id'] = Endereco::where('id', $value)->first()->id;
+        }
+    }
+    /**
+     * Ele retorna o relacionamento com a tabela de endereços
+     * @return Endereco
+     */
     public function enderecoRelationship()
     {
         return $this->belongsTo(Endereco::class, 'contato_id');
     }
-
+    /**
+     * Ele retorna o relacionamento com a tabela de telefones
+     * @return Telefone
+     */
     public function telefoneRelationship()
     {
         return $this->hasMany(Telefone::class, 'contato_id');
     }
+    /**
+     * Ele retorna o relacionamento de muitos para muitos da model contato com model categoria.
+     * @return Categoria
+     */
     public function categoriaRelationship()
     {
-        return $this->belongsToMany(Categoria::class, 'contatos_has_categorias' , 'categoria_id', 'contato_id');
+        return $this->belongsToMany(Categoria::class, 'contato_has_categoria' , 'categoria_id', 'contato_id');
 
     }
+    /**
+     *
+     */
+
 }
 
