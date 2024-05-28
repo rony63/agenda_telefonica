e5675a2dae3aef4ee99f0b7011cc0041e1ec2fac
rony63 <ronymcall@gmail.com>
2024-05-28 09:22:29 -0300

    Instalação do tailwind, e correção das funcionalidade dos botões.

--- resources/views/contatos/index.blade.php
+++ resources/views/contatos/index.blade.php
@@ -4,42 +4,62 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
-        <table>
-            <tr>
-                <th>Nome</th>
-                <th>Telefone</th>
-                <th>Logradouro</th>
-                <th>Cidade</th>
-                <th>Número</th>
-                <th>Categoria</th>
-                <th>Ações</th>
-            </tr>
-            {{-- {{dd($contatos[3]->endereco->logradouro)}} --}}
-            @foreach ( $contatos as $contato)
-                <tr>
-                    <td>{{$contato->nome}}</td>
-                    <td>
-                        @foreach ( $contato->telefone as $telefone)
-                            {{$telefone->numero}}
-                            {{$telefone->tipo}}
-                        @endforeach
-                    </td>
+        <div>
+            <h1>Contatos</h1>
+            <h1 class="text-3xl font-bold underline">
+                Hello world!
+              </h1>
+                <table >
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Telefone</th>
+                            <th>Logradouro</th>
+                            <th>Cidade</th>
+                            <th>Número</th>
+                            <th>Categoria</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
 
-                    <td>{{$contato->endereco->logradouro}}</td>
-                    <td>{{$contato->endereco->cidade}}</td>
-                    <td>{{$contato->endereco->numero}}</td>
-                    <td>
-                        @foreach ( $contato->categoria as $categoria )
-                            {{$categoria->nome}}
-                        @endforeach
-                    </td>
-                    <td><a href="{{route('contatos.show', $contato->id)}}">Visualizar</a></td>
+                    {{-- {{dd($contatos[3]->endereco->logradouro)}} --}}
+                @foreach ( $contatos as $contato)
+                    <tr>
+                        <td>{{$contato->nome}}</td>
+                        <td>
+                            @foreach ( $contato->telefone as $telefone)
+                                {{$telefone->numero}}
+                                {{$telefone->tipo}}
+                            @endforeach
+                        </td>
 
-                </tr>
-            @endforeach
+                        <td>{{$contato->endereco->logradouro}}</td>
+                        <td>{{$contato->endereco->cidade}}</td>
+                        <td>{{$contato->endereco->numero}}</td>
+                        <td>
+                            @foreach ( $contato->categoria as $categoria )
+                                {{$categoria->nome}}
+                            @endforeach
+                        </td>
+                        <td><a href="{{route('contatos.show', $contato->id)}}">Visualizar</a></td>
+                        <td>
+                            <form action="/destroy/{{ $contato->id }}" method="POST">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit">Deletar</button>
+                            </form>
+                        </td>
 
-        </table>
+                        <td><a href="{{route('contatos.edit', $contato->id)}}">Editar</a></td>
+
+                    </tr>
+                @endforeach
+
+            </table>
+        </div>
+        <button><a href="{{route('contatos.create')}}">Novo</a></button>
     </body>
 </html>
