a1fda0c1e501d5c8523e29a45acce7defdf8a916
rony63 <ronymcall@gmail.com>
2024-05-28 13:20:43 -0300

    adicionando alguns estilos

--- resources/views/contatos/index.blade.php
+++ resources/views/contatos/index.blade.php
@@ -7,16 +7,17 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body class="bg-gray-200 p-5">
-        <div class="">
-            <div>
+        <div class="bg-white pt-6 shadow rounded-md">
+            <div class="px-4">
                 <h1 class="text-lg leading-6 font-medium text-gray-900">
                     Contatos
                 </h1>
             </div>
-            <table class="table-auto">
-                    <thead>
+            <div class="mt-6">
+                <table class="w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-200">
                         <tr>
-                            <th>Nome</th>
+                            <th class="px-6 py-3 text-left text">Nome</th>
                             <th>Telefone</th>
                             <th>Logradouro</th>
                             <th>Cidade</th>
@@ -27,39 +28,40 @@
                     </thead>
 
                     {{-- {{dd($contatos[3]->endereco->logradouro)}} --}}
-                @foreach ( $contatos as $contato)
-                    <tr>
-                        <td>{{$contato->nome}}</td>
-                        <td>
-                            @foreach ( $contato->telefone as $telefone)
-                                {{$telefone->numero}}
-                                {{$telefone->tipo}}
-                            @endforeach
-                        </td>
+                    @foreach ( $contatos as $contato)
+                        <tr>
+                            <td>{{$contato->nome}}</td>
+                            <td>
+                                @foreach ( $contato->telefone as $telefone)
+                                    {{$telefone->numero}}
+                                    {{$telefone->tipo}}
+                                @endforeach
+                            </td>
 
-                        <td>{{$contato->endereco->logradouro}}</td>
-                        <td>{{$contato->endereco->cidade}}</td>
-                        <td>{{$contato->endereco->numero}}</td>
-                        <td>
-                            @foreach ( $contato->categoria as $categoria )
-                                {{$categoria->nome}}
-                            @endforeach
-                        </td>
-                        <td><a href="{{route('contatos.show', $contato->id)}}">Visualizar</a></td>
-                        <td>
-                            <form action="/destroy/{{ $contato->id }}" method="POST">
-                                @method('DELETE')
-                                @csrf
-                                <button type="submit">Deletar</button>
-                            </form>
-                        </td>
+                            <td>{{$contato->endereco->logradouro}}</td>
+                            <td>{{$contato->endereco->cidade}}</td>
+                            <td>{{$contato->endereco->numero}}</td>
+                            <td>
+                                @foreach ( $contato->categoria as $categoria )
+                                    {{$categoria->nome}}
+                                @endforeach
+                            </td>
+                            <td><a href="{{route('contatos.show', $contato->id)}}">Visualizar</a></td>
+                            <td>
+                                <form action="/destroy/{{ $contato->id }}" method="POST">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit">Deletar</button>
+                                </form>
+                            </td>
 
-                        <td><a href="{{route('contatos.edit', $contato->id)}}">Editar</a></td>
+                            <td><a href="{{route('contatos.edit', $contato->id)}}">Editar</a></td>
 
-                    </tr>
-                @endforeach
+                        </tr>
+                    @endforeach
 
-            </table>
+                </table>
+            </div>
         </div>
         <button><a href="{{route('contatos.create')}}">Novo</a></button>
 
